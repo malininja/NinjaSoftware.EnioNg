d2c34213f1db9ede2ca26abfed1ef0164d81c35e
malininja <mamasic@gmail.com>
2017-11-05 20:08:42 +0100

    - predlošci izvještaja dodani u projekt (csproj) - exception logging

--- NinjaSoftware.EnioNg.Web/Global.asax.cs
+++ NinjaSoftware.EnioNg.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NinjaSoftware.EnioNg.CoolJ.EntityClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,17 @@ namespace NinjaSoftware.EnioNg.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+
+            if (exception != null)
+            {
+                var adapter = Helpers.Helper.GetDataAccessAdapter();
+                ErrorEntity.LogException(adapter, exception);
+            }
         }
     }
 }
